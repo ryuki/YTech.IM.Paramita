d5ec6ba7cb70539a9cb735d06a502abfa77e619b
fachrial <firman.fachrial@gmail.com>
2011-04-24 18:13:39 +0700

    add UnitTypeController.cs
    Signed-off-by: fachrial <firman.fachrial@gmail.com>

---
+++ app/YTech.IM.Paramita.Web.Controllers/Master/UnitTypeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using SharpArch.Core;
+using SharpArch.Web.NHibernate;
+using YTech.IM.Paramita.Core.RepositoryInterfaces;
+
+namespace YTech.IM.Paramita.Web.Controllers.Master
+{
+    [HandleError]
+    public class UnitTypeController : Controller
+    {
+        private readonly IMUnitTypeRepository _mUnitTypeRepository;
+        public UnitTypeController(IMUnitTypeRepository mUnitTypeRepository)
+        {
+            Check.Require(mUnitTypeRepository != null, "mUnitTypeRepository may be not null");
+            _mUnitTypeRepository = mUnitTypeRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Transaction]
+        public ActionResult ListForSubGrid(string id)
+        {
+            var unitTypes = _mUnitTypeRepository.GetByUnitTypeId(id);
+        }
+    }
+}
